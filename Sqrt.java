f5815c8fb67b10ab9387f2c1622c308727ef1343
manjugoutham <gouthamy219@gmail.com>
2021-12-13 13:16:55 +0530

    Added Day6 Junit programs

--- Sqrt.java
+++ Sqrt.java
@@ -1,20 +1,27 @@
 package junit;
 
+import java.util.Scanner;
+
 public class Sqrt
 {
 
 	public static void main(String[] args) {
 
-        double c = Double.parseDouble(args[0]);
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter non-negative Number : ");
+        double num = sc.nextDouble();
+        double c = num;
 
         double epsilon = 1e-15;
 
         double t = c;
 
-        while (Math.abs(t - c / t) > epsilon * t) {
+        while (Math.abs(t - c / t) > epsilon * t)
+        {
             t = (c / t + t) / 2.0;
         }
 
         System.out.println(t);
     }
 }
+
